55b20f4c2ecc496003f68e48218379cc96985d93
Kam141 <kamilafadhil@gmail.com>
2025-03-02 21:42:25 +0700

    JS3_Praktikum 6_Implementasi Eloquent ORM

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserModel;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    //
+    public function index()
+    {
+        //menambahkan data user dengan eloquent model
+        // $data = [
+        //     'username' => 'customer-1',
+        //     'name' => 'Pelanggan',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 4
+        // ];
+
+        $data = [
+            'name' => 'Pelanggan Pertama',
+        ];
+        UserModel::where('username', 'customer-1')->update($data);
+
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+    }
+}
