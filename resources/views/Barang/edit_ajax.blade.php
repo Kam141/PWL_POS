61db2e3c35242ab05587170bfc2e673b0b4dfa13
Kam141 <kamilafadhil@gmail.com>
2025-03-25 20:44:49 +0700

    barang tapi masih ada bug

---
+++ resources/views/Barang/edit_ajax.blade.php
@@ -0,0 +1,116 @@
+@empty($barang)
+    <div id="modal-master" class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Kesalahan</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="alert alert-danger">
+                    <h5><i class="icon fas fa-ban"></i> Kesalahan!!!</h5>
+                    Data yang Anda cari tidak ditemukan.
+                </div>
+                <a href="{{ url('/barang') }}" class="btn btn-warning">Kembali</a>
+            </div>
+        </div>
+    </div>
+@else
+    <form action="{{ url('/barang/' . $barang->barang_id . '/update_ajax') }}" method="POST" id="form-edit">
+        @csrf
+        @method('PUT')
+        <div id="modal-master" class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Edit Data User</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    
+                    <div class="form-group">
+                        <label >Kode Barang</label>
+                        <input type="text" class="form-control" id="barang_kode" name="barang_kode" required>
+                        <small id="error-barang_kode" class="error-text form-text text-danger"></small>
+                    </div>
+                    <div class="form-group">
+                        <label >Nama Barang</label>
+                        <input type="text" class="form-control" id="barang_nama" name="barang_nama" required>
+                        <small id="error-barang_nama" class="error-text form-text text-danger"></small>
+                    </div>
+                    <div class="form-group">
+                        <label >Harga Beli</label>
+                        <input type="number" class="form-control" id="harga_beli" name="harga_beli" required>
+                        <small id="error-harga_beli" class="error-text form-text text-danger"></small>
+                    </div>
+                    <div class="form-group">
+                        <label >Harga Jual</label>
+                        <input type="number" class="form-control" id="harga_jual" name="harga_jual" required>
+                        <small id="error-harga_jual" class="error-text form-text text-danger"></small>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" data-dismiss="modal" class="btn btn-warning">Batal</button>
+                    <button type="submit" class="btn btn-primary">Simpan</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
+
+    <script>
+        $(document).ready(function() {
+            $("#form-edit").validate({
+                rules: {
+                    
+                    barang_kode: { required: true, minlength: 3, maxlength: 10 },
+                    barang_nama: { required: true, minlength: 1, maxlength: 100 },
+                    harga_beli: { required: true, number: true },
+                    harga_jual: { required: true, number: true },
+                },
+                submitHandler: function(form) {
+                    $.ajax({
+                        url: form.action,
+                        type: form.method,
+                        data: $(form).serialize(),
+                        success: function(response) {
+                            if (response.status) {
+                                $('#myModal').modal('hide');
+                                Swal.fire({
+                                    icon: 'success',
+                                    title: 'Berhasil',
+                                    text: response.message
+                                });
+                                dataBarang.ajax.reload();
+                            } else {
+                                $('.error-text').text('');
+                                $.each(response.msgField, function(prefix, val) {
+                                    $('#error-' + prefix).text(val[0]);
+                                });
+                                Swal.fire({
+                                    icon: 'error',
+                                    title: 'Terjadi Kesalahan',
+                                    text: response.message
+                                });
+                            }
+                        }
+                    });
+                    return false;
+                },
+                errorElement: 'span',
+                errorPlacement: function(error, element) {
+                    error.addClass('invalid-feedback');
+                    element.closest('.form-group').append(error);
+                },
+                highlight: function(element, errorClass, validClass) {
+                    $(element).addClass('is-invalid');
+                },
+                unhighlight: function(element, errorClass, validClass) {
+                    $(element).removeClass('is-invalid');
+                }
+            });
+        });
+    </script>
+@endempty
