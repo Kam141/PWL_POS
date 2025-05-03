4f63480f5bf5e25783092c10cdf813fd60662ffa
Kam141 <kamilafadhil@gmail.com>
2025-05-03 22:46:15 +0700

    JS 10_tugas bagian m_barang

--- app/Models/BarangModel.php
+++ app/Models/BarangModel.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class BarangModel extends Model
 {
@@ -13,10 +14,17 @@ class BarangModel extends Model
     protected $table = 'm_barang'; // Nama tabel di database
     protected $primaryKey = 'barang_id'; // Primary key tabel
 
-    protected $fillable = ['barang_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual', 'stok', 'kategori_id'];
+    protected $fillable = ['barang_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual', 'stok', 'kategori_id', 'image'];
 
     public function kategori(): BelongsTo
     {
         return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
+    }
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn($image) => $image ? asset('storage/barang/' . $image) : null,
+        );
     }
 }
